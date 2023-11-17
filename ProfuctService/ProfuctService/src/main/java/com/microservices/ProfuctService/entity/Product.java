31d86707bb787634dfcd0c0f6741bfa6b0e3b60b
Sumit Thapliyal <thapliyal.sumitth@gmail.com>
2023-11-17 16:00:39 +0530

    Added new Files

--- ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/entity/Product.java
+++ ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/entity/Product.java
@@ -1,6 +1,7 @@
 package com.microservices.ProfuctService.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import javax.persistence.*;
 @Table(name = "product")
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Product
 {
     @Id
--- ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/service/ProductService.java
+++ ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/service/ProductService.java
@@ -1,6 +1,9 @@
 package com.microservices.ProfuctService.service;
 
+import com.microservices.ProfuctService.model.ProductRequest;
+
 public interface ProductService
 {
 
+    long addProduct(ProductRequest productRequest);
 }
