aa33cbdcafc079470924d8b3a27c7d94b625be57
Sumit Thapliyal <thapliyal.sumitth@gmail.com>
2023-11-18 12:04:49 +0530

    Added new Files

--- ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/ProductServiceApplication.java
+++ ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/ProductServiceApplication.java
@@ -2,6 +2,7 @@ package com.microservices.ProfuctService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class ProductServiceApplication {
