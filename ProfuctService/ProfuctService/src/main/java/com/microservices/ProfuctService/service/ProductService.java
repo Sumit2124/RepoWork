c6945a65f1c1ecab96299f8ac72c1ba50c641490
Sumit Thapliyal <thapliyal.sumitth@gmail.com>
2023-12-18 11:41:58 +0530

    Deleted files

--- ProfuctService/ProfuctService/src/main/java/com/microservices/ProfuctService/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.ProfuctService.service;
-
-import com.microservices.ProfuctService.model.ProductRequest;
-
-public interface ProductService
-{
-
-    long addProduct(ProductRequest productRequest);
-}
